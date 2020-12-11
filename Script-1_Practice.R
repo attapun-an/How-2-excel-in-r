dd85e013f7ba22ae9fa3dcd27658148a5ecb65cb
attapun-an <attapunanivat@gmail.com>
2020-12-11 17:04:24 +0700

    pun, answer generating failed

--- Script-1_Practice.R
+++ Script-1_Practice.R
@@ -2,32 +2,38 @@
 # written by attapun-an
 
 # INSTRUCTIONS
-# run all the code before the excercise section)
+# run all the code before the exercise section
 
-# Install tidyr and dplyr (you only have to do this once)
-install.packages('dplyr')                                                       # you can delete these lines afterwards
+# Install packages (you only have to do this once)
+install.packages('dplyr')
 install.packages('tidyr')
 install.packages('readr')
+install.packages('rstudioapi')  #this is for lazy people to set the working directory with code
 # Load the packages 
 library('dplyr')                                                               
-library('tidyr')  
+library('tidyr')
 library('readr')
 
 
 # Set working directory to source file location 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))                     # this means the files we link will be 
                                                                                 # relative to the location of our script
-
 # Load the data 
 HS_data <- read.csv('data/human-substitute.csv')
-
 
 # head(HS_data) tail(HS_data) are normally used but we don't have that many rows
 HS_data
 str(HS_data)
 
+# Ensure that depth and test subjects are characters and not 
+HS_data$depth <- as.character(HS_data$depth)
+HS_data$test_subject <- as.character(HS_data$test_subject)
 
-# Make sure all these  
+# this just generates the answers so you can check if your answer is right or not
+source("data/checker-script.R")
+generate_ans(HS_data)
+
+# Make sure you run all of the code on top
 
 # EXCERISE ----
 # Instructions:
@@ -37,22 +43,21 @@ str(HS_data)
 
 # you can preview what the answer looks like if you change Usr to Ans
 # For example
-Ans_f1 
+Ans_f1 #this shows the answer for f1
 
-
-# Example: parsing numbers
-parse_number("X2001")
+# Separating ----
 
 # Example: separating 
 separate(HS_data, dimensions_WH, into = c("width", "height"), sep = "x")
-# sep is the character/symbol that 
 
-# Example: pasting
-paste("Hello", "world", )
+# Task: separate test_subject into "type" and "number" (example below)
+# Type   Number
+# Pillow      1
+# Pillow      2
 
-# Task 
-
-
+Usr_sep1 <- ____ # put your answer into this object
+  
+check_separate(Usr_s1)
 
 # Example
 filter(HS_data, material == 'iron') # filter only rows that have iron as the material
@@ -60,8 +65,7 @@ filter(HS_data, material == 'iron') # filter only rows that have iron as the mat
 # Task 1: Filter only rows that have wood as their material
 Usr_f1 <- filter(HS_data, ____ )
 
-
-# Task 2: Filter the data to only include materials that are wood and sentience is FALSE
+# Task 2: Filter the data to only include materials that are wood and sentient is FALSE
 Usr_f2 <- ____
 
 # Filter only rows that have brick or glass as their materials 
@@ -70,18 +74,57 @@ Usr_f3 <- ____
 
 check_filter(Usr_f1, Usr_f2, Usr_f3)
 
-# Select only type and depth 
-Usr_td <- select(HS_data, ____)
+
+# Select ----
+
+# Example: select only the columns type and hs1
+select(HS_data, c(type, hs1))
+
+# Example: select everything but those columns
+select(HS_data, -c(type, hs1))
+
+# Select only material and depth 
+Usr_sel1 <- ____
 
 # Select everything except material
-Usr_nonMat <- ____
+Usr_sel2 <- ____
 
 
-# Create a new column 
+check_select(Usr_s1, Usr_s2)
+# Mutating and More ---- 
+
+# Example: parsing numbers (see what each does)
+parse_number("X2001")
+
+parse_number("100,02,34")
+
+# Example: pasting (see what each does)
+paste("Hello", "world", sep = " ")
+
+paste("15","20","21", sep = ",")
+
+paste("15","12","20", sep = "") # warning (the output is a string, cannot use for maths)
+
+# Example: create a copy of the depth column using mutate()
+mutate(HS_data, depth_copy = depth)
+
+# Example: create a new column called hs_early that adds up hs1 and hs2
+mutate(HS_data, hs_early = hs1 + hs2)
+
+# Task 1: create a new column called hs_late_average that adds up hs3 and h4 and 
+# and divides them by two
+Usr_m1 <- 
 
 
+# Task 2: create a new column called dimensions_WHD with the by combining 
+# dimensions_WH and depth 
+# (hint: use the paste() function)
+Usr_m2 <- 
+  
 
-
+check_mutate(Usr_m1, Usr_m2)
+## THE RENAME SECTION GOES HERE 
+  # this is to avoid any confusion with renaming columns
 
 
 # Examples ----
@@ -98,9 +141,6 @@ parse_number("23,34")
 temp <- select(HS_data, c(dimensions_WH)) # create new object with only the dimensions column
 separate()
 
-# PASTE
-
-
 # separate dimensions_WH, "x" is the separator
 separate(HS_data, dimensions_WH, into = c("width", "height"),sep = "x")
 
@@ -111,6 +151,7 @@ HS_data$height <- as.numeric(HS_data$width)
 
 
 # PASTE 
+
 
 # FILTER
 
--- data/checker-script.R
+++ data/checker-script.R
@@ -1,1 +1,60 @@
-checker script
+# checker script
+
+checker <- function(flag){
+  if(TRUE){
+    return("Correct")
+  }
+  else{
+    return("Not quite")
+  }
+}
+
+generate_ans <- function(HS_data){
+  # library(tidyr)
+  # library(dplyr)
+  # library(readr)
+  
+  # Task: separate test_subject into "type" and "number" (example below)
+  # Type   Number
+  # Pillow      1
+  # Pillow      2
+  Ans_sep1 <- separate(HS_data, test_subject, into = c("type", "number"), sep = "_") # put your answer into this object
+  
+  # Task 1: Filter only rows that have wood as their material
+  Ans_f1 <- filter(HS_data, material == "wood")
+  
+  # Task 2: Filter the HS_data to only include materials that are wood and sentient is FALSE
+  Ans_f2 <- filter(HS_data, material == "wood" & sentient == FALSE)
+  
+  # Filter only rows that have brick or glass as their materials 
+  Ans_f3 <- filter(HS_data, material %in% c("brick", "glass"))
+  
+  # Select only material and depth 
+  Ans_sel1 <- select(HS_data, c(material, depth))
+  
+  # Select everything except material
+  Ans_sel2 <- select(HS_data, -c(material))
+  # Task 1: create a new column called hs_late_average that adds up hs3 and h4 and 
+  # and divides them by two
+  Ans_m1 <- mutate(HS_data, hs_late_average = (hs3 +h4)/2)
+    
+    
+  # Task 2: create a new column called dimensions_WHD with the by combining 
+  # dimensions_WH and depth 
+  # (hint: use the paste() function)
+  Ans_m2 <- mutate(HS_data, dimensions_WHD = paste(dimensions_WH, depth, sep = ""))
+  
+}
+
+check_separate <- function(Usr_sep1){
+  flag_sep1 <- FALSE
+  if (identical(Usr_sep1, Ans_sep1)){
+    flag_sep1 <- TRUE
+  }
+  sep_reply <- paste("Task1:",checker(flag_sep1))
+  
+  return(paste(sep_reply, sep = "\n"))
+}
+
+# check_filter(Usr_f1, Usr_f2, Usr_f3)
+
